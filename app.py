cf6c8cea7e0edb2a194dc59c28143aaabe9f6d0b
kauan <kauanchoc@gmail.com>
2024-09-09 12:34:53 -0300

    adicionar filme ok

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
 
 API_URL = 'http://127.0.0.1:5000/api/movies'
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def indx():
+    if request.method == 'POST':
+        title = request.form['title']
+        genre = request.form['genre']
+        requests.post(API_URL, json={'title':title, 'genre':genre})
+        return redirect('/')
+        pass
     response = requests.get(API_URL)
-    return render_template('index.html')
+    if response.status_code == 200:
+            movies = response.json()
+    else:
+            movies = []
+    return render_template('index.html', movies=movies)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
